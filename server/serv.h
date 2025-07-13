9e5d16e8a0e72e380cece72a0864215f4316addd
Đạt Phạm <dat.pt204@gmail.com>
2025-07-13 14:56:42 +0700

    first commit

---
+++ server/serv.h
@@ -0,0 +1,27 @@
+#ifndef SERV_H
+#define SERV_H
+
+#include <arpa/inet.h>
+
+
+#define PORT 9000
+#define BUFFERSIZE 4096
+#define BACKLOG 10
+#define LINE_SIZE 512
+
+typedef struct Serv {
+    int serv_fd;
+    int opt;
+    struct sockaddr_in addr;
+    socklen_t socklen;
+} Serv;
+
+void Serv_setup(Serv *serv);      //
+int Serv_accept_cli(Serv *serv);  //
+void Serv_run(Serv *serv);        //
+void Serv_del(Serv *serv);        //
+
+static int Serv_send_res(int cli_fd, JsonReq* res);
+static JsonReq* Serv_recv_req(int cli_fd);
+
+#endif
